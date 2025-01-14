bc65de6501e48c46359b1137bc0e5342f1ab61ad
BOYANG ZHANG <bzhang523@twhuang-desktop-06>
2025-01-14 15:15:04 -0600

    BLT has BUG, ctest failed

--- examples/omp_dt.cpp
+++ examples/omp_dt.cpp
@@ -4,6 +4,12 @@ int main() {
 
   gdiamond::gDiamond exp(100, 100, 100); 
 
-  exp.update_FDTD_omp_dt(16, 16, 16, 5, 100);
+  exp.update_FDTD_seq(100);
+  exp.update_FDTD_omp_dt(31, 31, 31, 10, 100);
+
+  if(!exp.check_correctness_omp_dt()) {
+    std::cerr << "error: omp_dt result wrong\n";
+    std::exit(EXIT_FAILURE);
+  }
   return 0;
 }
--- unittests/checker_omp_dt.cpp
+++ unittests/checker_omp_dt.cpp
@@ -6,7 +6,7 @@
 // --------------------------------------------------------
 // Testcase: check correctness of openmp compared to single thread implementation 
 // --------------------------------------------------------
-TEST_CASE("check correctness of openmp" * doctest::timeout(300)) {
+TEST_CASE("check correctness of openmp, BLX=BLY=BLZ=16, BLT=5" * doctest::timeout(300)) {
 
   gdiamond::gDiamond exp(100, 100, 100); 
 
@@ -16,6 +16,29 @@ TEST_CASE("check correctness of openmp" * doctest::timeout(300)) {
   REQUIRE(exp.check_correctness_omp_dt() == true);
 
 }
+
+TEST_CASE("check correctness of openmp, BLX=BLY=BLZ=25, BLT=10" * doctest::timeout(300)) {
+
+  gdiamond::gDiamond exp(100, 100, 100); 
+
+  exp.update_FDTD_seq(100);
+  exp.update_FDTD_omp_dt(25, 25, 25, 10, 100);
+
+  REQUIRE(exp.check_correctness_omp_dt() == true);
+
+}
+
+TEST_CASE("check correctness of openmp, BLX=BLY=BLZ=31, BLT=20" * doctest::timeout(300)) {
+
+  gdiamond::gDiamond exp(100, 100, 100); 
+
+  exp.update_FDTD_seq(100);
+  exp.update_FDTD_omp_dt(31, 31, 31, 20, 100);
+
+  REQUIRE(exp.check_correctness_omp_dt() == true);
+
+}
+
 
 
 
