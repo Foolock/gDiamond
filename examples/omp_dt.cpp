42bd64e928fe39696ce911d3a8971a73d34a2463
BOYANG ZHANG <bzhang523@twhuang-desktop-06>
2025-01-16 01:19:14 -0600

    finish omp_dt, BLT and BLX has constraints

--- examples/omp_dt.cpp
+++ examples/omp_dt.cpp
@@ -5,13 +5,13 @@ int main() {
   size_t Nx = 100;
   size_t Ny = 100;
   size_t Nz = 100;
-  size_t num_timesteps = 5;
+  size_t num_timesteps = 1000;
   gdiamond::gDiamond exp(Nx, Ny, Nz); 
 
   exp.update_FDTD_seq(num_timesteps);
-  size_t BLX = 7;
-  size_t BLY = 7;
-  size_t BLZ = 7;
+  size_t BLX = 16;
+  size_t BLY = 16;
+  size_t BLZ = 16;
   size_t BLT = 5;
   exp.update_FDTD_omp_dt(BLX, BLY, BLZ, BLT, num_timesteps);
 
